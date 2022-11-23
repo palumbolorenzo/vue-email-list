4536c65a1b435967930566b94ab2b549f9a20353
palumbolorenzo <palumbolorenzo97@gmail.com>
2022-11-23 09:34:23 +0100

    provvisorio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 new Vue ({
     el: '#app',
     data: {
-        mail: 0,
+        mail: [],
     },
     methods: {
         getMail(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            for(let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(axiosResponse => {
                     console.log(axiosResponse);
                     this.mail = axiosResponse.data.response;
                 })
+            }
         }
     }
 })
